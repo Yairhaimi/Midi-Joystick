fb493a5a5d0eb62ceacd360cbadabf11f8b29f37
Yair Haimi <yairhaimi19@gmail.com>
2025-05-11 11:20:12 +0300

    feat: added display class, showing white rectangle

--- code.py
+++ code.py
@@ -3,48 +3,22 @@
 #
 #
 
-import time, usb_midi, adafruit_midi, board, busio, displayio, adafruit_displayio_ssd1306, terminalio, os
+import time, usb_midi, adafruit_midi, board
 
 from adafruit_simplemath import map_range
-from adafruit_display_text import label
 
-from i2cdisplaybus import I2CDisplayBus
 from analogio import AnalogIn
 from adafruit_midi.note_on import NoteOn
 from adafruit_midi.note_off import NoteOff
 from adafruit_midi.pitch_bend import PitchBend
 from adafruit_midi.control_change import ControlChange
 
+from display_handling import display
 
-displayio.release_displays()
-i2c = busio.I2C(board.GP15, board.GP14)
-display_bus = I2CDisplayBus(i2c, device_address=0x3C)
-display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
 
-# Make the display context
-splash = displayio.Group()
-display.root_group = splash
+oled = display(board.GP15, board.GP14, 128, 64, device_address=0x3C)
 
-color_bitmap = displayio.Bitmap(128, 64, 1)
-color_palette = displayio.Palette(1)
-color_palette[0] = 0xFFFFFF  # White
-
-bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
-splash.append(bg_sprite)
-
-# Draw a smaller inner rectangle
-inner_bitmap = displayio.Bitmap(118, 54, 1)
-inner_palette = displayio.Palette(1)
-inner_palette[0] = 0x000000  # Black
-inner_sprite = displayio.TileGrid(inner_bitmap, pixel_shader=inner_palette, x=5, y=5)
-splash.append(inner_sprite)
-
-# Draw a label
-text = "Hello World!"
-text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00, x=28, y=28)
-splash.append(text_area)
-
-while True:
+while(True):
     pass
 
 # pot_1 = AnalogIn(board.GP26)
